30c414887c04d12b495f15f445d6a01641f1f24f
hshehriyaralam <hshehriyaralam@gmail.com>
2024-05-06 01:16:41 -0600

    Conversions

---
+++ Data Type Conversion/app.js
@@ -0,0 +1,69 @@
+//COnvrt to Number
+// let score = "33";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+
+// let score2  = 33;
+// let ValueString = String(score2)
+// console.log(typeof ValueString);
+
+
+// let score = "33abc";
+// let value = Number(score)
+// console.log(typeof value);
+
+
+// let score = null;
+// let score = undefined;
+// let score = true;
+// // let score = false;
+// let score = "sherry";
+
+
+
+
+// let value = Number(score)
+// console.log(typeof score);
+// console.log(typeof (score));
+// console.log(typeof value);
+// console.log(value);
+
+
+// "33" => 33
+// "33abc" => NaN , but type is => number
+// true => 1; false => 0;
+
+
+
+// Convert to Boolean
+// 1 => true
+// 0 => false
+// "" => false
+// "sherry" => true
+
+
+// jab number ko Boolean main convert karenge
+// let LoggedIn = 1;
+// let BooleanLogedin = Boolean(LoggedIn)
+// console.log( typeof BooleanLogedin);
+
+
+// let LoggedIn = 0;
+// let BooleanLogedin = Boolean(LoggedIn)
+// console.log(BooleanLogedin);
+
+// let LoggedIn = "";
+// let BooleanLogedin = Boolean(LoggedIn);
+// console.log(BooleanLogedin);
+
+//Convert to Strings
+// let some = 33;
+// let somenumber = String(some);
+// console.log(somenumber);
+// console.log( typeof somenumber);
+
+
