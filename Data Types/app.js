99cc51fabb10d199165a916bec6176d84e262e83
hshehriyaralam <hshehriyaralam@gmail.com>
2024-05-06 00:26:42 -0600

    Data-Types

---
+++ Data Types/app.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as newer version
+// JS ka engin browser main chupa hua hai
+
+// alert( 3 + 3 )
+/// code readability should be high;
+
+
+let name = "shahmeer";
+let age = 20;
+let isLogin = false;
+
+
+// Data Types   
+// number => 2 to power 53  >> ek range hai jitna memory address main milta hai
+// bigint => huge number store >> jb number bohat zyada bara hai to ye use karenge
+// string => ""
+//boolean => true / false
+// null => standalone value
+// symbol => unique
+// object
+
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null); // Object
+console.log(typeof undefined); // undefined
+
+
---
+++ let, var and const/app.js
@@ -0,0 +1,26 @@
+const accountId = 52531;
+let accountEmail = "hshehriyaralam@gmail.com";
+var accountPassword = "shahmeer234";
+
+accountCIty = "Karachi"
+
+// accountId = 2342;
+
+accountEmail = "shahmeer@gmail.com"
+accountPassword = "sherry6645632"
+accountCIty = "Lahore"
+// console.log(accountEmail, accountPassword, accountCIty);
+// console.table([accountEmail, accountPassword, accountCIty]);
+
+/*
+ Prefer not to use var becuase of issue in block scope and functional scope
+*/
+
+
+/* Variable without keyword bh declare ho sakra hai */ 
+
+
+
+
+
+
