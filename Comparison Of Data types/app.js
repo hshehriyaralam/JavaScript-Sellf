c85e08e95ccd89ad720d33599611e9c5784c1f7c
hshehriyaralam@gmail.com <hshehriyaralam@gmail.com>
2024-05-09 14:37:59 -0600

    Summary of Data Types

---
+++ Comparison Of Data types/app.js
@@ -0,0 +1,34 @@
+// console.log("Comparison Of Data Types");
+
+// console.log(2 > 1); // true
+// console.log(2 >= 1); // true
+// console.log(2 <= 1);// false 
+// console.log(2 >= 1); // true
+// console.log(2 != 1); // tur
+
+
+// But problem jb Hoti HAi jab same Data Type ko comparemnh krte 
+// console.log("2" > 1);   // true 
+// console.log("02" > 1);  // true 
+// JS ne Apke is "2" ko Convert kr dya number main  ....most of the time aisa hota hai  diffrent Data type ke comparison main apka predictable result nh deta hai..
+// Comparison krte waqt dhyan rahe Data Types ek hi ho
+ 
+// console.log(null > 0); // false 
+// console.log(null >= 0); // true  value conversion ka problem ajata hai
+// console.log(null == 0); // false 
+
+
+// undefined main kch bh ho value false hi ati hai/
+// console.log(undefined == 0); 
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+
+
+
+
+
+
+
+
---
+++ Data Types Summary/app.js
@@ -0,0 +1,52 @@
+// console.log("hello World");
+// Data Type summary
+// Data Type 2 types ke HOte Hain , Kis tarah se Data store Kya GAya HAi and kis Tarah se Ap isee access kr sakte  hain
+// 1) Primitive => Call by value | 
+// 7  types : String, Number, Boolean, null, Undefined, Symbol, BigInt
+
+// const score = 100;
+// // const scoreValue = 10.2; => Decimal ke lye koi special nh hai 
+// let userEmail;
+// console.log(userEmail);
+
+// const userId = Symbol('123')
+// const anotherId  = Symbol('123')
+// console.log(anotherId === userId); // false => symbol uniqness ke lye hai
+// console.log(userId);
+// console.log(anotherId);
+
+
+
+// const BigIntNumber = 537255735476253257352757157265n;
+// console.log(typeof BigIntNumber);
+// // Data Tyoe bigINt Banane Ke lye bs last main "n" add kr dete hain ,, but  BigInt itna usable nh hai ye
+
+
+
+
+// 2) Non Primitive | Refernece Type | Call By Reference
+// Array, Object, Function
+
+
+// const heros = ["fahad", "Saeed", "Babar"];
+// let Obj = {
+//     name: "sherry",
+//     age: 20,
+// }
+// function myfunction (){
+//     console.log("Sherry");
+// }  
+// myfunction();
+
+// function ka Data Type Function hi hota but wo "Object function" hota hai
+// Jitne BH Non Primitve DAta Types hai inke data type Object hi hote hain,,,,,but function ke data type ko Object Function Bola jata hai
+// Symbol ka Data Type Symbol hi ata hai
+
+
+
+
+
+
+
+
+
