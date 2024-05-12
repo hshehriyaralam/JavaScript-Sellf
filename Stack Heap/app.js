0e7c7472b5ae163bdf5dd66f263cc2199ad60e61
hshehriyaralam <hshehriyaralam@gmail.com>
2024-05-12 03:29:44 -0600

    Stack and Heap

---
+++ Stack Heap/app.js
@@ -0,0 +1,10 @@
+//Java Script main Memory kasie work karti hai .......?
+// starting main jaise ke C languages main  as a programmer memory ko reserve krna and memory ko free krna dono apka kam hota tHa but current modern language  main like Java Script me apko memory ka itna control nh dya jata hai sb automatic garbidge collection hoTi hai...
+
+// *** Modern Languages => Rust, Go(Golang), swift, Kotlin, TypesScript
+// *** Mid modern langauges => Java, Java Script, Python, C#, PHP, Ruby
+
+// 2 Types of Data Types => 1) Primitive 2) Non-Primitive
+// 2 Types of Memory => 1) Stack    2) Heap
+// Stack => Primitive Data Types 
+console.log();
