9e733dc0f23c2f14467f3b7f3e060c7dc948e0a5
hshehriyaralam <hshehriyaralam@gmail.com>
2024-06-25 14:24:27 -0600

    Globl and Block Scope

---
+++ 17) Scope/app.js
@@ -0,0 +1,33 @@
+// Scope 
+// Curly Braces {} Har Programing Language main Scope hain
+// Object main curly  Braces {} Objects Declaration hai
+// Function , array and conditions ke sath curly  Braces {} scope hain
+
+
+// let a = 10;
+// const b = 20;
+// var c = 30;
+// if (true){
+//     let a = 10  // Block Scope
+//     const b = 20 // Block Scope
+//     var c = 30 // Global Scope
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+/* 
+********************** Summary ***************************
+01) Curly Braces {} all programmin languages is scope 
+02) Objects Curly Braces it's mean Objects Declaration
+03) Curly Braces {} with functions, array and conditions are Scope
+04) let => Block Scope
+05) const => Block Scope
+06) var  => Global Scope
+*/ 
+
+
+
+
+
