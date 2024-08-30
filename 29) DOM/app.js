89e162731603f02bfbcdce90a1beeabedad4651c
hshehriyaralam <hshehriyaralam@gmail.com>
2024-08-31 00:29:57 +0500

    DOM

---
+++ 29) DOM/app.js
@@ -0,0 +1,21 @@
+console.log("app.js");
+
+// HTML ke attribute ko Javascript se control kar sakte with DOM
+
+// window browser ka global Object hai 
+console.log(window);
+// document main just HTML ke Tag  access hogi 
+console.log(window.document);
+
+// HTML Collection, Node Collection ye sare alag hai 
+
+
+
+
+
+
+
+
+
+
+
