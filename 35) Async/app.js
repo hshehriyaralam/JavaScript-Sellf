5f5c237d67472fe15f09eef948775257678354cf
hshehriyaralam <hshehriyaralam@gmail.com>
2024-09-04 23:05:55 +0500

    async theory

---
+++ 35) Async/app.js
@@ -0,0 +1,45 @@
+// Async 
+// * Javascript syncronous hai it's mean line by line code run 
+// * javascript single threaded language ek hi thread main sb code execute hoga 
+
+
+
+// ** Execution Context 
+// * execute one line of code at a time 
+// console.log(Map);
+// console.log(Map)
+
+
+// ** Blocking Code            |         VS          | Non-Blocking Code 
+// Block the flow of program   |                     | Do not block execution       
+// Read File Sync              |                     | Read file async 
+//                             |                     |
+// apko main pani la kr de raha|                     | apko pani laa kr de rha hun ja tk ap
+// hun jab tk koi kam nh krna  |                     | apna kam kr lo 
+
+
+
+/* ************************** Summary ***********************
+01) javascript syncronous language hai
+02) javascript single thraded language hia
+03) Javacsript execute one line of code at a time 
+04) Blocking Code : Block the flow of program | Read file sync 
+05) Non-Blocking Code : Do not block execution | Read file async 
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
