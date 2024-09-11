60708c6da105cf1f4ac9aec3d0601d81be526074
hshehriyaralam <hshehriyaralam@gmail.com>
2024-09-12 00:16:39 +0500

    fetch

---
+++ 41) Fetch/app.js
@@ -0,0 +1,9 @@
+// fetch : global "fetch" method hai 
+// ek process start krta hai kisi bh resource ko fetch krne  ke lye network ke uppar 
+// promise return karta hai 
+
+
+
+// promise se apne request kari or whan se error code aa gya 404 , ye resolve m ayega ya reject m ayega ya fetch m ayega ?
+
+// wo as a response  milega wo error nh h ... error tab jab request kr hi nh paya browser main 
